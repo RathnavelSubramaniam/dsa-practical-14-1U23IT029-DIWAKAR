a0e9ead0d1794546e87f795e5e717c77c72a07a4
1U23IT029-DIWAKAR <144431139+1U23IT029-DIWAKAR@users.noreply.github.com>
2025-03-06 08:53:25 +0000

    Done14

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,65 +1,71 @@
 import java.util.*;
-class ArrayStack{	
-public char[] stack;
-public int capacity;
-protected int top = -1;
-public ArrayStack(int len) {
-    capacity=len;
-stack = new char[len]; 
-}
-public int size()
-{
-    return (top+1);
-}
-public void push(char data) 
-{
-if (isFull())
-System.out.println("Stack is Overflow. Not possible to insert in Full stack");
-else
-stack[++top] = data;
-}
-public void pop() 
-{
-char data='0';
-if (isEmpty())
-{
-System.out.println("Stack is Underflow. No elements to be popped in Empty Stack");
 
-}
-else
-{
-data = stack[top];
-top--;
-}
-System.out.print(data);
-}
-public boolean isFull()
-{
-    return (size()==capacity);
-}
-public boolean isEmpty()
-{
-    return (top<0);
-}
+public class ArrayStack {
+    private char[] stack;
+    private int capacity;
+    private int top = -1;
 
-public void display()
-{
-    if(isEmpty())
-    System.out.println("Stack is Underflow. No elements to display in Empty Stack");
-    else
-    {
-     System.out.println("Stack Elements");
-    for(int i=top;i>=0;i--)
-    System.out.println(stack[i]);
+    // Task 3: Create a Parameterized Constructor
+    public ArrayStack(int len) {
+        capacity = len;
+        stack = new char[len];
     }
-}
 
-public static void main(String[] args)
-{
-    
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the string to reverse:"); 
-    String str=sc.nextLine();
-      //TYPE YOUR CODE HERE
-}
-}
+    // Task 4: Create a Function "Size"
+    public int size() {
+        return (top + 1);
+    }
+
+    // Task 5: Create the Push Function
+    public void push(char data) {
+        if (isFull()) {
+            System.out.println("Stack is Overflow. Not possible to insert in Full stack");
+        } else {
+            stack[++top] = data;
+            System.out.println("Element is inserted in the stack using Push");
+        }
+    }
+
+    // Task 6: Create the Pop Function
+    public char pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is Underflow. No elements to be popped in Empty Stack");
+            return '\0'; // Return null character if stack is empty
+        } else {
+            return stack[top--];
+        }
+    }
+
+    // Task 7: Create the isFull Function
+    public boolean isFull() {
+        return (size() == capacity);
+    }
+
+    // Task 8: Create the isEmpty Function
+    public boolean isEmpty() {
+        return (top < 0);
+    }
+
+    // Task 9: Create the Display Function
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Stack is Underflow. No elements to display in Empty Stack");
+        } else {
+            System.out.println("Stack Elements:");
+            for (int i = top; i >= 0; i--) {
+                System.out.println(stack[i]);
+            }
+        }
+    }
+
+    // Main Function to Reverse a String
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string to reverse:");
+        String str = sc.nextLine();
+
+        // Create an ArrayStack with a size equal to the length of the input string
+        ArrayStack stack = new ArrayStack(str.length());
+    }}
+
+        // Push all
